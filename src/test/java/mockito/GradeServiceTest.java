80c7829c5218f1e0be41f2b9dc7950a2f7cb038d
fightwing <2608379678@qq.com>
2020-09-16 00:46:45 +0800

    stubbing passed

--- src/test/java/mockito/GradeServiceTest.java
+++ src/test/java/mockito/GradeServiceTest.java
@@ -3,12 +3,17 @@ package mockito;
 import mock.DoorPanel;
 
 import mock.SecurityCenter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import stub.GradeService;
+import stub.GradeSystem;
+
 import static org.mockito.Mockito.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,11 +24,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GradeServiceTest {
 
     @Test
-    public void shouldVerifyDoorIsClosed() {
-        DoorPanel mockDoorPanel = mock(DoorPanel.class);
-        doNothing().when(mockDoorPanel).close();
-        SecurityCenter securityCenter = new SecurityCenter(mockDoorPanel);
-        securityCenter.switchOn();
-        verify(mockDoorPanel,times(1));
+    void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100(){
+        GradeSystem mockGradeSystem = mock(GradeSystem.class);
+        GradeService gradeService = new GradeService(mockGradeSystem);
+        when(mockGradeSystem.gradesFor(123L)).thenReturn(Arrays.asList(90.0, 80.0, 100.0));
+        double result = gradeService.calculateAverageGrades(123L);
+        Assertions.assertEquals(90,result);
+
     }
+
 }
--- src/test/java/mockito/SecurityCenterTest.java
+++ src/test/java/mockito/SecurityCenterTest.java
@@ -1,8 +1,23 @@
 package mockito;
 
+import mock.DoorPanel;
+import mock.SecurityCenter;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
 /**
  * @author Boyu Yuan
  * @date 2020/9/15 23:59
  */
 public class SecurityCenterTest {
+    @Test
+    public void shouldVerifyDoorIsClosed() {
+        DoorPanel mockDoorPanel = mock(DoorPanel.class);
+        doNothing().when(mockDoorPanel).close();
+        SecurityCenter securityCenter = new SecurityCenter(mockDoorPanel);
+        securityCenter.switchOn();
+        verify(mockDoorPanel,times(1));
+    }
 }
